bcc6bfa06ee62962756defc6ba2273d2c405bdad
flyq <flyq951@gmail.com>
2019-02-27 16:40:11 +0800

    Update eos.signature.js

--- public/js/eos.signature.js
+++ public/js/eos.signature.js
@@ -1,5 +1,5 @@
 var chainId = 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906';
-var endpoint = 'https://mainnet.eoscannon.io';
+var endpoint = 'https://api.eosbeijing.one';
 
 var eos = Eos({
     keyProvider: '',
